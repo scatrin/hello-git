79cc88b7977e1d2f87e04f1bbbe0db8b8f68910b
David Ahlbeck <david.ahlbeck@scania.com>
2016-10-03 13:35:13 +0200

    added constructor for outputstream

--- Hello.java
+++ Hello.java
@@ -1,4 +1,13 @@
+import java.lang.OutputStream;
+
 public class Hello {
+  /**
+   * A constructor.
+   */
+  public Hello(final OutputStream outputStream) {
+    this.outputStream = outputStream;
+  }
+
   public void hello(final String message) {
     System.out.println("Hello " + message);
   }
